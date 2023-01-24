ce324546b068326d35fdab14f365fa47a5c1ceab
OLDx21 <71173909+OLDx21@users.noreply.github.com>
2023-01-24 10:43:02 +0200

    Initial commit

---
+++ src/main/java/com/mdemydovych/webmessaging/WebMessagingApplication.java
@@ -0,0 +1,13 @@
+package com.mdemydovych.webmessaging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebMessagingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebMessagingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mdemydovych/webmessaging/WebMessagingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mdemydovych.webmessaging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebMessagingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
